c5256bf41492bedbd19b81c3f7d9807709d471a9
Nirajan Kunwor <nirajan.kunwor604@gmail.com>
2022-12-07 23:32:47 +0545

    feat: mobile responsive & home page added

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,16 @@
-import Header from '../components/Header.js'
+import Head from 'next/head';
+import Navbar from '../components/Navbar.js';
+import HomePage from '../components/Home.js';
 
 export default function Home() {
   return (
-    <Header />
+    <div>
+      <Head>
+        <title>Cleaning Website</title>
+      </Head>
+      <Navbar />
+      <HomePage />
+    </div>
+
   )
 }
